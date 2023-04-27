cb7e7770e70b1557c5f4d523928f6c78b41d4574
fuwenchao <miniimg@163.com>
2023-04-27 17:17:42 +0800

    add remotefilemodel

---
+++ model/remotefilemodel.cpp
@@ -0,0 +1,6 @@
+#include "remotefilemodel.h"
+
+RemoteFileModel::RemoteFileModel()
+{
+
+}
---
+++ model/remotefilemodel.h
@@ -0,0 +1,11 @@
+#ifndef REMOTEFILEMODEL_H
+#define REMOTEFILEMODEL_H
+
+
+class RemoteFileModel
+{
+public:
+    RemoteFileModel();
+};
+
+#endif // REMOTEFILEMODEL_H
