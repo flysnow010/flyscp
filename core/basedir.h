da78d0b54a2a00c55e371a8fa5716e2a4ff3f3ee
fuwenchao <miniimg@163.com>
2023-05-11 16:42:20 +0800

    add function: home and root

--- core/basedir.h
+++ core/basedir.h
@@ -10,6 +10,8 @@ public:
     virtual void setDir(QString const& dir) = 0;
     virtual QString dir() const = 0;
     virtual void cd(QString const& dir) = 0;
+    virtual QString home() const = 0;
+    virtual QString root() const = 0;
 };
 
 #endif // BASEDIR_H
