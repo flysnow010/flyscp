d2a794ea5025f47b149fee3d0ed903b485263590
fuwenchao <miniimg@163.com>
2023-05-12 18:44:52 +0800

    add dirhistory

---
+++ core/dirhistory.h
@@ -0,0 +1,36 @@
+#ifndef DIRHISTORY_H
+#define DIRHISTORY_H
+#include <QStringList>
+
+class DirHistory
+{
+public:
+    DirHistory();
+
+    inline void add(QString const& dir)//???const
+    {
+        int index = dirs_.indexOf(dir);
+        if(index >= 0)
+            dirs_ << dirs_.takeAt(index);
+        else
+            dirs_ << dir;
+    }
+
+    inline QString find(QString const& prefix)
+    {
+        for(auto dir = dirs_.rbegin(); dir != dirs_.rend(); ++dir)
+        {
+            if(dir->startsWith(prefix))
+                return *dir;
+        }
+        return QString("%1:/").arg(prefix);
+    }
+
+    inline QStringList const& dirs() const { return dirs_; }
+
+    inline void setDirs(QStringList const& dirs) { dirs_ = dirs; }
+private:
+    QStringList dirs_;
+};
+
+#endif // DIRHISTORY_H
