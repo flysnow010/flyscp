8966368e2c43315c51086ed1ebdf6e8c1363e083
飞雪 <miniimg@163.com>
2023-06-23 15:56:03 +0800

    add many interface

--- core/basedir.h
+++ core/basedir.h
@@ -1,6 +1,7 @@
 #ifndef BASEDIR_H
 #define BASEDIR_H
 #include <QString>
+#include <QFont>
 class BaseDir
 {
 public:
@@ -12,6 +13,7 @@ public:
     virtual void cd(QString const& dir) = 0;
     virtual QString home() const = 0;
     virtual QString root() const = 0;
+    virtual void refresh() = 0;
     virtual void showHeader(bool isShow) = 0;
     virtual void showCurrentDir(bool isShow) = 0;
     virtual void showDeskNavigationButton(bool isShow) = 0;
@@ -21,6 +23,18 @@ public:
     virtual void showToolTips(bool isShow) = 0;
     virtual void showParentInRoot(bool isShow) = 0;
     virtual void setDirSoryByTime(bool isOn) = 0;
+    virtual void setRenameFileName(bool isOn) = 0;
+    virtual void showAllIconWithExeAndLink(bool isShow) = 0;
+    virtual void showAllIcon(bool isShow) = 0;
+    virtual void showStandardIcon(bool isShow) = 0;
+    virtual void showNoneIcon(bool isShow) = 0;
+    virtual void showIconForFyleSystem(bool isShow) = 0;
+    virtual void showIconForVirtualFolder(bool isShow) = 0;
+    virtual void showOverlayIcon(bool isShow) = 0;
+    virtual void fileIconSize(int size) = 0;
+    virtual void fileFont(QFont const& font) = 0;
+    virtual void setItemColor(QString const& fore, QString const& back, QString const& alternate) = 0;
+    virtual void setItemSelectedColor(QString const& back, QString const& mark, QString const&cursor) = 0;
 };
 
 #endif // BASEDIR_H
